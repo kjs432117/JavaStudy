5c33cf9953cd705d092bf9ccdd2698f67ebb1eed
admin <admin@DESKTOP-NIE3S4J>
2021-10-22 14:04:25 +0900

    Java study p.268 Q.3 2021/10/22

--- chapter06/src/com/yedam/java/ch0604/MemberService.java
+++ chapter06/src/com/yedam/java/ch0604/MemberService.java
@@ -2,11 +2,20 @@ package com.yedam.java.ch0604;
 
 public class MemberService {
 	
+	String id;
+	String password;
+	
+	boolean isLogin() {
+		if(id == "hong" & password == "12345") {
+			System.out.println("로그인 성공");
+			return true;
+		} System.out.println("로그인 실패");
+		return false;
+	}
 	
 	
+	void logout() {
+		System.out.println("로그아웃 되었습니다.");
+	}
 	
-//	boolean login() {
-	
-	
-
 }
