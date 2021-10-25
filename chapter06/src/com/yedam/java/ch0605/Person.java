30666fae531da83a97dd7cc6bd59c333598a17df
admin <admin@DESKTOP-NIE3S4J>
2021-10-25 15:17:03 +0900

    Java study 2021/10/25

---
+++ chapter06/src/com/yedam/java/ch0605/Person.java
@@ -0,0 +1,22 @@
+package com.yedam.java.ch0605;
+
+public class Person {
+	final static String nation = "Korea"; //final 정적 필드
+	final String ssn; //final 인스턴스 필드
+	String name;
+	
+	public Person(String ssn, String name) {
+		this.ssn = ssn;
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ chapter06/src/com/yedam/java/feedback2/Product.java
@@ -0,0 +1,29 @@
+package com.yedam.java.feedback2;
+
+import java.util.Scanner;
+
+public class Product {
+	   String name;
+	   int price;
+	   
+	   public Product(String name, int price) {
+	      this.name = name;
+	      this.price = price;
+	   }
+	   
+	   public String getName() {
+	      return this.name;
+	   }
+	   
+	   public int getPrice() {
+	      return this.price;
+	   }
+	   
+	   public void setName(String name) {
+	      this.name = name;
+	   }
+	   
+	   public void setPrice(int price) {
+	      this.price = price;
+	   }
+	}
